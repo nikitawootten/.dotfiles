aa9d954a35d30c915cbbff16354dd980cc9e787a
Nikita Wootten <nikita.wootten@gmail.com>
2021-07-19 15:37:32 -0400

    Misc. fixes discovered while using the tiny laptop

--- roles/zsh/files/.config/zshrc.d/04-keybindings.zsh
+++ roles/zsh/files/.config/zshrc.d/04-keybindings.zsh
@@ -1,2 +1,14 @@
+# ctrl+(shift+)tab
 bindkey "^[[1;5C" forward-word
 bindkey "^[[1;5D" backward-word
+# delete key
+bindkey '^[[3' prefix-2
+bindkey '^[[3~' delete-char
+# home/end
+bindkey '^[[H' beginning-of-line
+bindkey '^[[F' end-of-line
+
+zmodload zsh/complist
+# shift+tab reverse autocomplete
+bindkey -M menuselect '^[[Z' reverse-menu-complete
+
